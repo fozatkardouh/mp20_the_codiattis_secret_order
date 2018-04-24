1f0f4ec1d2ebc3e5a14688fa47e37882d004a29b
Fozat Kardouh <fozatkardouh@gmail.com>
2018-04-24 11:42:33 +0200

    added empty project

---
+++ src/main/java/at/refugeescode/mp20_the_codiattis_secret_order/Mp20TheCodiattisSecretOrderApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp20_the_codiattis_secret_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp20TheCodiattisSecretOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp20TheCodiattisSecretOrderApplication.class, args);
+	}
+}
